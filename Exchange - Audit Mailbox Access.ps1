91986369cb1838305ef8e87ecb15cf8bd3939a11
Get-LocalUser <harisonkerkof@outlook.com>
2025-08-22 06:44:21 -0700

    Update Exchange - Audit Mailbox Access.ps1

--- Exchange - Audit Mailbox Access.ps1
+++ Exchange - Audit Mailbox Access.ps1
@@ -17,4 +17,4 @@ $admin = Read-Host "Enter your admin account (e.g., john.doe@example.com)"
 $user = Read-Host "Enter the user's account name (e.g., john.doe)"
 
 Connect-ExchangeOnline -UserPrincipalName $admin -ShowBanner:$false
-Get-Mailbox -ResultSize Unlimited | Get-MailboxPermission -User $user | Format-Table User, Identity, AccessRights
+Get-EXOMailbox -ResultSize Unlimited | Get-EXOMailboxFolderPermission -User $user | Format-Table User, Identity, AccessRights
