28479f90e18a298a8c794c4b499b468033cc06cf
Get-LocalUser <121067700+Get-LocalUser@users.noreply.github.com>
2026-01-29 12:30:10 -0800

    Update Exchange - Create New Room Resource.ps1

--- Exchange - Create New Room Resource.ps1
+++ Exchange - Create New Room Resource.ps1
@@ -15,8 +15,11 @@ New-Mailbox `
     -PrimarySmtpAddress "$RoomName@example.com"
 
 Set-CalendarProcessing $RoomName `
-    -AutomateProcessing AutoAccept `
-    -BookingWindowInDays 365
+    -AutomateProcessing AutoAccept ` # Automatically accept meeting requests as long as no policy says otherwise.
+    -BookingWindowInDays 365 ` # How many days in advance you can book the room.
+    -AllBookInPolicy $False ` # Not everyone can autobook. Only users allowed explicity in the 'BookinPolicy'.
+    -AllRequestInPolicy $True ` # Anyone in the org can request the room but may need delegate approval if setup. If $false, only people in the BookInPolicy can request. if not, it'll be auto denied.
+    -AllRequestOutOfPolicy $False # users who are out of policy cannot request the room.
 
 Set-Mailbox $RoomName `
-    -ResourceCapacity $RoomCapacity
+    -ResourceCapacity $RoomCapacity # Number of people the room can hold
