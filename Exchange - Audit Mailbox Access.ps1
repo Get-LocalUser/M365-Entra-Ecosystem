6cecba862fe74b37dacd38f2fd7b41ca093c4028
Get-LocalUser <121067700+Get-LocalUser@users.noreply.github.com>
2026-01-29 12:57:15 -0800

    Delete Exchange - Audit Mailbox Access.ps1

--- Exchange - Audit Mailbox Access.ps1
+++
@@ -1,5 +0,0 @@
-$admin = Read-Host "Enter your admin account (e.g., john.doe@example.com)"
-$user = Read-Host "Enter the user's account name (e.g., john.doe)"
-
-Connect-ExchangeOnline -UserPrincipalName $admin -ShowBanner:$false
-Get-EXOMailbox -ResultSize Unlimited | Get-EXOMailboxFolderPermission -User $user | Format-Table User, Identity, AccessRights
