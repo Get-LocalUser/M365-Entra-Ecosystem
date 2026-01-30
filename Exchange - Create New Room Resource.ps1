40846ea11c65e41075dc3f6446ac6c7d43b14b29
Get-LocalUser <121067700+Get-LocalUser@users.noreply.github.com>
2026-01-30 09:05:16 -0800

    .

--- Exchange - Create New Room Resource.ps1
+++ Exchange - Create New Room Resource.ps1
@@ -10,6 +10,8 @@ param(
 $Alias = ($RoomName -replace '\s','')
 $PrimarySmtp = "$Alias@example.com"
 
+Write-Host "This may take a few minutes" -ForegroundColor Yellow
+
 # Create room mailbox
 New-Mailbox `
     -Name $Alias `
@@ -17,6 +19,8 @@ New-Mailbox `
     -Room `
     -DisplayName $RoomName `
     -PrimarySmtpAddress $PrimarySmtp
+
+    Start-Sleep -Seconds 15
 
 # Configure calendar processing
 Set-CalendarProcessing $Alias `
